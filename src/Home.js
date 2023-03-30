5af65348cd57e2d654d8ca217b3baac3be453ddd
Max Maddux <maxmaddux@gmail.com>
2023-03-30 08:40:36 -0700

    Try to align center on phones

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,8 @@ import Movies from "./Movies";
 const Home = () => {
   return (
     <div className="block bg-gray-200 items-center m-auto">
-      <main className="flex w-3/4 m-auto bg-gray-200 items-center overflow-auto">
-        <div className="m-auto p-4 w-full ">
+      <main className="flex w-3/4 m-auto bg-gray-200 items-center">
+        <div className="m-auto p-4 w-full h-3/4 items-center overflow-auto">
             <h1 className="logo mb-3 w-3/5">
               TV Show <span className="logo-span mb-3">Database!</span>
             </h1>
