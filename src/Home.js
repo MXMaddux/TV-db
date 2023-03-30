de94aa5271edf9cd0505c6f13609295f685628d2
Max Maddux <maxmaddux@gmail.com>
2023-03-30 14:04:55 -0700

    Add vertical scrollbar

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,9 @@ import Form from "./SearchForm";
 import Movies from "./Movies";
 const Home = () => {
   return (
-    <div className="block bg-gray-200 items-center m-auto">
-      <main className="flex w-3/4 m-auto bg-gray-200 items-center">
-        <div className="m-auto p-4 w-full h-3/4 items-center overflow-auto">
+    <div className="flex bg-gray-200 items-center m-auto w-screen h-screen">
+      <main className="flex w-5/6 m-auto bg-gray-200 items-center">
+        <div className="gray-div m-auto p-4 w-full items-center overflow-y-auto">
             <h1 className="logo mb-3 w-3/5">
               TV Show <span className="logo-span mb-3">Database!</span>
             </h1>
