054911dc4a1191bee617bc7f42e0eb8cb627ef95
Max Maddux <maxmaddux@gmail.com>
2023-03-31 05:30:40 -0700

    change font for genres, cast

--- src/Movies.js
+++ src/Movies.js
@@ -23,27 +23,28 @@ const Movies = () => {
   }
   return (
     <section className="bg-gray-200 height-3/4">
-        <div className="bg-gray-500 p-4 h-3/4 w-full">
-          <img src={image} alt={showName} />
-          <div className="mt-4">
-            <p className="text-base text-white">
-              <span className="text-teal-300 mr-2">Title: </span>
-              {showName}
-            </p>
-            <p className="text-base text-white">
-              <span className="text-teal-300 mr-2">Summary: </span>
-              {summary}
-            </p>
-            <p className="text-base text-white">
-              <span className="text-teal-300 mr-2">Premier Date: </span>
-              {premierDate}
-            </p>
-            <p className="text-base text-white">
-              <span className="text-teal-300 mr-2">End Date: </span>
-              {endDate}
-            </p>
-          </div>
-          <div>
+      <div className="bg-gray-500 p-4 h-3/4 w-full">
+        <img src={image} alt={showName} />
+        <div className="mt-4">
+          <p className="text-base text-white">
+            <span className="text-teal-300 mr-2">Title: </span>
+            {showName}
+          </p>
+          <p className="text-base text-white">
+            <span className="text-teal-300 mr-2">Summary: </span>
+            {summary}
+          </p>
+          <p className="text-base text-white">
+            <span className="text-teal-300 mr-2">Premier Date: </span>
+            {premierDate}
+          </p>
+          <p className="text-base text-white">
+            <span className="text-teal-300 mr-2">End Date: </span>
+            {endDate}
+          </p>
+        </div>
+        <div>
+          <p className="text-base">
             <span className="text-teal-300 mr-2"> Genres: </span>
             {genres.map((gen, index) => {
               return (
@@ -54,18 +55,21 @@ const Movies = () => {
                 </div>
               );
             })}
-          </div>
-          <div className="">
-            <span className="text-teal-300 text-l mr-2"> Cast: </span>
-            {cast.map((member, index) => {
-              return (
-                <p className="text-base text-white" key={index}>
-                  {member.person.name} as {member.character.name},
-                </p>
-              );
-            })}
-          </div>
+          </p>
         </div>
+        <div >
+        <p className="text-base">
+          <span className="text-teal-300 text-l mr-2"> Cast: </span>
+          {cast.map((member, index) => {
+            return (
+              <p className="text-base text-white" key={index}>
+                {member.person.name} as {member.character.name},
+              </p>
+            );
+          })}
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
