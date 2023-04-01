7cf1ae5f2c1c2b9d10007ca468fb35eac97f6b0d
Max Maddux <maxmaddux@gmail.com>
2023-04-01 13:27:48 -0700

    Add search any tv show

--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,20 @@ import Form from "./SearchForm";
 import Movies from "./Movies";
 const Home = () => {
   return (
-    <div className="flex bg-gray-200">
+    <div className="bg-gray-200">
       <main className=" main-div flex  bg-gray-200">
         <div className="gray-div p-4 w-full ">
+          <div className="flex items-center">
             <h1 className="logo mb-3 w-3/5">
               TV Show <span className="logo-span mb-3">Database!</span>
             </h1>
+            <h3>Search any TV Show!</h3>
+          </div>
           <Form />
           <Movies />
         </div>
       </main>
-    </div> 
+    </div>
   );
 };
 
---
+++ src/SingleMOvie.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import { API_ENDPOINT } from './context'
+
+// url_params = "type=series&t=${term}"
+
+const SingleMovie = () => {
+  return <h2>single movie</h2>
+}
+
+export default SingleMovie
